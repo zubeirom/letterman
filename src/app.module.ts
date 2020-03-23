d49d19c7a16590852f0d0a28b97e058a6fcf3a7f
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-03-23 03:11:22 +0100

    setup auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LetterModule } from './letter/letter.module';
 import { AccountModule } from './account/account.module';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 require('dotenv').config();
 
 @Module({
-    imports: [LetterModule, MongooseModule.forRoot(process.env.LOCAL_DB), AccountModule],
+    imports: [LetterModule, MongooseModule.forRoot(process.env.LOCAL_DB), AccountModule, AuthModule],
     controllers: [AppController],
     providers: [AppService],
 })
